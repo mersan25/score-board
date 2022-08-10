857ad8fbfc626f7a2808835923fdde3b9282d021
mer205 <mersansufinanal@gmail.com>
2022-08-11 00:11:47 +0100

    score-board

---
+++ main.js
@@ -0,0 +1,34 @@
+let homeEl=document.getElementById("home-el")
+console.log(homeEl)
+let home = 18
+let guestEl=document.getElementById("guest-el")
+let guest = 16
+
+function add1(){
+  home += 1
+  homeEl.innerText =home
+}
+
+function add2(){
+  home += 2
+  homeEl.innerText =home
+}
+
+function add3(){
+  home += 3
+  homeEl.innerText =home
+}
+function add4(){
+  guest += 1
+  guestEl.innerText =guest
+}
+
+function add5(){
+  guest += 2
+  guestEl.innerText =guest 
+}
+
+function add6(){
+ guest += 3
+  guestEl.innerText = guest
+}
